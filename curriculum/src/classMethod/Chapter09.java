9e9d5b6ce4094c05e8374aec0a1d325d705c5fa3
Kengo <funeace@gmail.com>
2020-06-19 20:11:28 +0900

    [Add]Chapter10

--- curriculum/src/classMethod/Chapter09.java
+++ curriculum/src/classMethod/Chapter09.java
@@ -4,7 +4,7 @@ public class Chapter09 {
     public static void main(String[] args) {
         // 燃費（Km/L）
         double fuelCost = 12.5;
-        // 残量（L）
+        // 残量（L）　
         double fuelAmount = 55.0;
         Car car = new Car(fuelCost, fuelAmount);
         car.move(20);
